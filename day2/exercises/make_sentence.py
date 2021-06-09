a82ebfd353f529458870624202819b8e9aa7c44e
Juan <jhermo@princeton.edu>
2021-06-09 12:24:51 -0400

    Edited make_sentence.py

--- day2/exercises/make_sentence.py
+++ day2/exercises/make_sentence.py
@@ -4,6 +4,6 @@ sentence = ''
 
 while word not in ['!', '?', '.']:
         word = input('Enter a word (. ! or ? to end):')
-        sentence += word + ' ':
+        sentence += word + ' '
             
 print(sentence)
